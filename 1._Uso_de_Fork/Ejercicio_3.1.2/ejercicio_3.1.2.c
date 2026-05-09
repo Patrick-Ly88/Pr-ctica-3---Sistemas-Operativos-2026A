dbc2cc4fe6b086f9b4f3671a336835864bdc6018
Karely <naomy.bombon@epn.edu.ec>
2026-05-09 00:47:21 +0000

    Ejercicio 3.1.2

---
+++ 1._Uso_de_Fork/Ejercicio_3.1.2/ejercicio_3.1.2.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main()
+{
+	int pid;
+	int i;
+	int variable = 0;
+
+	pid = fork();
+
+	switch(pid){
+
+		case -1:
+			perror("Error al crear el proceso");
+			return 1;
+
+		case 0: //Proeso hijo
+
+			{
+				FILE *archivo = fopen("log.txt", "w");
+				if(archivo == NULL) exit(1);
+
+				for(i = 0; i < 10; i++){
+					fprintf(archivo, "Hijo lee valor de variable: %d\n", variable);
+				}
+
+				fclose(archivo);
+				exit(0);
+			}
+			break;
+
+		default: //Proceso padre
+
+			for(i = 0; i < 10; i++){
+				variable += 10;
+				printf("Padre incrementa variable a: %d\n", variable);
+			}
+
+			wait(NULL);
+			break;
+
+	}
+
+	return 0;
+}
